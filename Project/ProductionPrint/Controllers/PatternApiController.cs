39aa7fc71c85ce5fb8edb4275fb87110d69a1776
DM <danushkads48@gmail.com>
2021-08-17 16:54:15 +0530

    update

---
+++ Project/ProductionPrint/Controllers/PatternApiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductionPrint.models;
+
+namespace ProductionPrint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatternApiController : ControllerBase
+    {
+        
+        [HttpPost("saveMaster")]
+        public DataTable SaveMaster(PatternCreate ptn)
+        {
+           
+            return ptn.Save_Master();
+        }
+        
+        [HttpGet("get_Mast_data")]
+        public DataSet Get_Mast_data()
+        {
+            PatternCreate ptn = new PatternCreate(CommonData.ConStr());
+            return ptn.Load_Mast_data();
+        }
+    }
+}
--- Project/ProductionPrint/models/PatternCreate.cs
+++ Project/ProductionPrint/models/PatternCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,9 +27,19 @@ namespace ProductionPrint.models
         public Guid suplier_id { get; set; } 
         public int isCore { get; set; } 
         public int isActive { get; set; }
+
+        public int id{ get; set; }
+        public string p_type { get; set; }
+        public string size { get; set; }
+        public string agegroup { get; set; }
+        public string catergory { get; set; }
+        public string gender { get; set; }
+        public string description { get; set; }
+        public int typ { get; set; } 
+        public Guid usr { get; set; } 
         private string connection { get; set; }
 
-        public PatternCreate()
+        public PatternCreate( string conn)
         {
             pattern_id = new Guid();
             pattern_name = "";
@@ -45,6 +56,41 @@ namespace ProductionPrint.models
             suplier_id = new Guid();
             isCore = 0;
             isActive = 0;
+
+            p_type = "";
+            size = "";
+            agegroup  = "";
+            catergory = "";
+            gender = "";
+            description = "";
+            typ = 0;
+            usr = new Guid();
+
+            connection = conn;
+        }
+        //
+        public DataSet Load_Mast_data()
+        {
+            {
+                var objDIc = new Dictionary<string, object>();
+                return (new DbAccess(CommonData.ConStr())).LoadDataSetBySP("Merchandiser_Loadt_Mast", objDIc);
+            }
+        }
+        public DataTable Save_Master()
+        {
+            {
+                var objDIc = new Dictionary<string, object> {
+                {"p_type",p_type},
+                {"size",size},
+                {"agegroup",agegroup},
+                {"catergory",catergory},
+                {"gender",gender},
+                {"description",description},
+                {"typ",typ},
+                {"usr",usr}
+            };
+                return (new DbAccess(CommonData.ConStr())).LoadDatatableBySP("_Merchandiser_pttninfo_MAST_Save", objDIc);
+            }
         }
 
 
